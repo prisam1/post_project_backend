681a3646d1f4f09873dbd2c1f4f7e43f4c5a2582
Pritam <pritamsamaddar840@gmail.com>
2025-07-10 11:01:34 +0530

    done

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+//   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+//   res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+//   next();
+// });
 
 mongoose
   .connect(process.env.MONGO_URI)
